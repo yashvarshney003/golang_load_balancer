4ceabebd0f90cc75eef4a0e29d90969a6701ccdd
yash003 <yash.varshney003@gmail.com>
2025-05-08 01:37:36 +0530

    Forward http request from golang to another server

--- server.go
+++ server.go
@@ -3,10 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
+// type backendserver struct {
+// 	url string
+// 	islive bool
+// }
+// type ServerPool struct {
+// 	backends []*Backend
+// 	current  uint64
+//   }
+  
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Go Web Server!")
+	fmt.Println("Request URL:", r.URL.String())
+	fmt.Println("Path:", r.URL.Path)
+	query := r.URL.Query()
+	fmt.Println("Query parameters:")
+    for key, values := range query {
+        for _, value := range values {
+            fmt.Printf("  %s = %s\n", key, value)
+        }
+    }
+	backendURL, _ := url.Parse("http://127.0.0.1:8000/")
+	proxy := httputil.NewSingleHostReverseProxy(backendURL)
+	proxy.ServeHTTP(w, r)
 }
 
 func main() {
